f5f2dac03c7cc73c2d71ad0e1a2632f14f4f725f
DebyGrey <demifashoro@gmail.com>
2023-06-23 09:21:25 +0100

    Redux basics

---
+++ src/components/CartItem.js
@@ -0,0 +1,42 @@
+import { ChevronDown, ChevronUp } from "../icons";
+import { removeItem, increase, decrease } from "../features/cart/cartSlice";
+import { useDispatch } from "react-redux";
+
+const CartItem = ({ id, img, title, price, amount }) => {
+  const dispatch = useDispatch();
+  return (
+    <article className="cart-item">
+      <img src={img} alt={title} />
+      <h4>{title}</h4>
+      <h4 className="item-price">${price}</h4>
+      <button className="remove-btn" onClick={() => dispatch(removeItem(id))}>
+        remove
+      </button>
+      <div>
+        <button
+          className="amount-btn"
+          onClick={() => {
+            dispatch(increase({ id }));
+          }}
+        >
+          <ChevronUp />
+          <p className="amount">{amount}</p>
+        </button>
+        <button
+          className="amount-btn"
+          onClick={() => {
+            if (amount === 1) {
+              dispatch(removeItem(id));
+              return;
+            }
+            dispatch(decrease({ id }));
+          }}
+        >
+          <ChevronDown />
+        </button>
+      </div>
+    </article>
+  );
+};
+
+export default CartItem;
